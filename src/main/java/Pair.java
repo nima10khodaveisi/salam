596efae525ea0be2cc4f3e625d9bf5df98c339fc
Nima <nima10khodaveisi@gmail.com>
2020-03-20 10:14:46 +0330

    Initial commit

---
+++ src/main/java/Pair.java
@@ -0,0 +1,17 @@
+public class Pair<T, T1> {
+    T a ;
+    T1 b ;
+
+    public T getKey() {
+        return a ;
+    }
+
+    public T1 getValue() {
+        return b ;
+    }
+
+    public Pair(T a , T1 b) {
+        this.a = a ;
+        this.b = b ;
+    }
+}
