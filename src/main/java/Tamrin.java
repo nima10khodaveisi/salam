ce5b61627d2859de36f1459764bcfd7eff5760c0
Nima <nima10khodaveisi@gmail.com>
2020-03-31 15:59:35 +0430

    finally

--- src/main/java/Tamrin.java
+++ src/main/java/Tamrin.java
@@ -8,6 +8,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
+
+import com.google.gson.Gson ;
+
+import java.io.*;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,14 +24,15 @@ public class Tamrin extends TelegramLongPollingBot {
     private String nameOfPlayList = null;
     private ArrayList<User> users = new ArrayList<>();
     private User curUser = null;
-    private ArrayList<Message> sent = new ArrayList<>() ;
+    // { { date , audio } , { chatId , messageId } }
+    private ArrayList<Pair<Pair<Long,String>,Pair<Long,Integer>>> sent = new ArrayList<>() ;
 
     public void clear_history() {
-        ArrayList<Message> history = curUser.getHistory();
+        ArrayList<Pair<Long,Integer>> history = curUser.getHistory();
         if (history.isEmpty())
             return;
-        for (Message message : history) {
-            DeleteMessage deleteMessage = new DeleteMessage(curUser.getChatId(), message.getMessageId());
+        for (Pair<Long,Integer> message : history) {
+            DeleteMessage deleteMessage = new DeleteMessage(curUser.getChatId(), message.getValue());
             try {
                 execute(deleteMessage);
             } catch (TelegramApiException e) {
@@ -38,35 +43,6 @@ public class Tamrin extends TelegramLongPollingBot {
         curUser.setHistory(history);
     }
 
-    /*
-
-    public void check_history_time() {
-        if(curUser == null) {
-            return ;
-        }
-        ArrayList<Message> history = curUser.getHistory() ;
-        if(history.isEmpty()) {
-            return;
-        }
-        ArrayList<Message> remove = new ArrayList<>() ;
-        for(Message message : history) {
-            Date now = new Date() ;
-            if(now.getTime() / 1000 - message.getDate() >= 30 * 60 * 60) {
-                remove.add(message) ;
-                String chatId = message.getChatId().toString();
-                Integer messageId = message.getMessageId() ;
-                DeleteMessage deleteMessage = new DeleteMessage(chatId , messageId) ;
-                try {
-                    execute(deleteMessage) ;
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        for(Message message : remove)
-            history.remove(message) ;
-        curUser.setHistory(history) ;
-    }*/
 
     public void sendMessage(String text) {
         SendMessage message = new SendMessage();
@@ -81,47 +57,96 @@ public class Tamrin extends TelegramLongPollingBot {
     }
 
     public void deleteSent() {
-        ArrayList<Message> remove = new ArrayList<>() ;
-        ArrayList<Message> add = new ArrayList<>() ;
+        setSent();
+        ArrayList<Pair<Pair<Long,String>,Pair<Long,Integer>>> remove = new ArrayList<>() ;
+        ArrayList<Pair<Pair<Long,String>,Pair<Long,Integer>>> add = new ArrayList<>() ;
         if(sent.isEmpty()) return ;
         for(int i = 0 ; i < sent.size() ; ++i) {
-            Message message = sent.get(i) ;
+            Pair<Pair<Long,String>,Pair<Long,Integer>> message = sent.get(i) ;
             long now = System.currentTimeMillis() ;
-            long messageTime = message.getDate() ;
+            long messageTime = message.getKey().getKey() ;
             if(now / 1000 - messageTime >= 20 * 60 * 60){
                 remove.add(message) ;
 
-                DeleteMessage deleteMessage = new DeleteMessage(message.getChatId(), message.getMessageId()) ;
+                DeleteMessage deleteMessage = new DeleteMessage(message.getValue().getKey(), message.getValue().getValue()) ;
                 SendAudio sendAudio = new SendAudio() ;
-                sendAudio.setAudio(message.getAudio().getFileId());
-                sendAudio.setChatId(message.getChatId());
+                sendAudio.setAudio(message.getKey().getValue());
+                sendAudio.setChatId(message.getValue().getKey());
                 try {
                     execute(deleteMessage) ;
                     Message message1 = execute(sendAudio) ;
-                    add.add(message1) ;
+                    add.add(new Pair(new Pair(message1.getDate(),message1.getAudio().getFileId()) , new Pair(message1.getChatId(),message1.getMessageId()))) ;
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
             }
         }
-        for(Message message : remove)
+        for(Pair<Pair<Long,String>,Pair<Long,Integer>>  message : remove)
             sent.remove(message) ;
-        for(Message message : add)
+        for(Pair<Pair<Long,String>,Pair<Long,Integer>>  message : add)
             sent.add(message) ;
+        updateSentFile() ;
+    }
+
+    public void updateSentFile() {
+        File file = new File("sent.json") ;
+        try {
+            file.createNewFile() ;
+            Gson gson = new Gson() ;
+            String json = gson.toJson(sent) ;
+            FileWriter fw = new FileWriter(file) ;
+            fw.write(json) ;
+            fw.flush();
+            fw.close() ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setSent() {
+        Gson gson = new Gson() ;
+        try {
+            sent = gson.fromJson(new FileReader(new File("sent.json")) , ArrayList.class) ;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         boolean alreadyUser = false;
+        /*
         for (User user : users) {
             if (user.getChatId() == update.getMessage().getChatId()) {
                 curUser = user;
                 alreadyUser = true;
                 break;
             }
+        }*/
+
+        File usersFile = new File("users") ;
+        usersFile.mkdir() ;
+        File []files = usersFile.listFiles() ;
+        for(File file : files) {
+            if(file.isFile()) {
+                long chatId = update.getMessage().getChatId() ;
+                String name = file.getName() ;
+                if(name.equals(String.valueOf(chatId) + ".json")) {
+                    alreadyUser = true ;
+                    Gson gson = new Gson() ;
+                    try {
+                        curUser = gson.fromJson(new FileReader(file) , User.class) ;
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                    break ;
+                }
+            }
         }
+
+
         if (!alreadyUser) {
-            System.out.println("shiiiit");
             curUser = new User(update.getMessage());
             users.add(curUser);
         }
@@ -166,7 +191,7 @@ public class Tamrin extends TelegramLongPollingBot {
                 return;
             }
             curUser.add(nameOfPlayList, update.getMessage());
-            curUser.addToHistory(update.getMessage());
+            curUser.addToHistory(new Pair(update.getMessage().getChatId(),update.getMessage().getMessageId()));
             System.out.println("add " + update.getMessage().getAudio().getTitle());
         } else if (command.equals("/get")) {
             curUser.setCommand(null);
@@ -175,6 +200,7 @@ public class Tamrin extends TelegramLongPollingBot {
             if (playList == null) {
                 return;
             }
+            clear_history();
             ArrayList<String> songs = playList.getSongs();
             for (String song : songs) {
                 System.out.println(song);
@@ -184,7 +210,9 @@ public class Tamrin extends TelegramLongPollingBot {
                 System.out.println("get " + name + " " + song);
                 try {
                     Message message = execute(sendAudio);
-                    sent.add(message) ;
+                    sent.add(new Pair(new Pair(message.getDate(),message.getAudio().getFileId()) , new Pair(message.getChatId(),message.getMessageId()))) ;
+                    updateSentFile() ;
+                    curUser.addToHistory(new Pair(message.getChatId(),message.getMessageId())) ;
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -8,8 +8,12 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.bots.AbsSender ;
 
+import com.google.gson.Gson ;
 
-
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class User {
@@ -17,21 +21,53 @@ public class User {
     private String command ;
     private long chatId ;
     private ArrayList<PlayList> playLists = new ArrayList<>() ;
-    private ArrayList<Message> history = new ArrayList<>() ;
+    private ArrayList<Pair<Long , Integer>> history = new ArrayList<>() ;
+    private File file ;
 
     public User(Message message) {
         if(message.getFrom().getUserName() != null) {
             name = message.getFrom().getUserName() ;
         } else {
-            name = message.getFrom().getFirstName() + " " + message.getFrom().getLastName() ;
+            name = message.getFrom().getFirstName() + "-" + message.getFrom().getLastName() ;
         }
         chatId = message.getChatId() ;
         command = null ;
+
+        createFile() ;
+        updateFile();
+    }
+
+    public User() { }
+
+    private void createFile( ) {
+        File users = new File("users") ;
+        users.mkdir() ;
+
+        file = new File("users" + File.separator + String.valueOf(chatId) + ".json") ;
+        try {
+            file.createNewFile() ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateFile() {
+        Gson gson = new Gson() ;
+        String json = gson.toJson(this) ;
+        try {
+            FileWriter fileWriter = new FileWriter(file) ;
+            fileWriter.write(json) ;
+            fileWriter.flush();
+            fileWriter.close() ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void createNewPlayList(String name) {
         PlayList playList = new PlayList(name) ;
         playLists.add(playList) ;
+        updateFile();
     }
 
     public void add(String name , Message message) {
@@ -44,6 +80,7 @@ public class User {
                 break ;
             }
         }
+        updateFile();
     }
 
     public PlayList getPlayList(String name) {
@@ -54,21 +91,19 @@ public class User {
         return null ;
     }
 
-    public void addToHistory(Message message) {
+    public void addToHistory(Pair<Long,Integer> message) {
         System.out.println("add to history");
         history.add(message) ;
+        updateFile();
     }
 
-    public void removeFromHistory(Message message) {
+    public void removeFromHistory(Pair<Long , Integer> message) {
         history.remove(message) ;
+        updateFile();
     }
 
     public long getChatId() {
         return chatId;
-    }
-
-    public void setChatId(long chatId) {
-        this.chatId = chatId;
     }
 
     public ArrayList<PlayList> getPlayLists() {
@@ -85,6 +120,7 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+        updateFile();
     }
 
     public String getCommand() {
@@ -93,13 +129,15 @@ public class User {
 
     public void setCommand(String command) {
         this.command = command;
+        updateFile();
     }
 
-    public ArrayList<Message> getHistory() {
+    public ArrayList<Pair<Long,Integer>> getHistory() {
         return history;
     }
 
-    public void setHistory(ArrayList<Message> history) {
+    public void setHistory(ArrayList<Pair<Long,Integer>> history) {
         this.history = history;
+        updateFile();
     }
 }
